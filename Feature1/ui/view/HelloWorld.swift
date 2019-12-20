a4045fbf7a72bf7cd55c3e2bf288a8cdf276f734
Eduardo Oliveira <eduardo.oliveira3d@gmail.com>
2019-12-20 14:11:07 -0300

    Tornando publico a view da feature 1

--- Feature1/ui/view/HelloWorld.swift
+++ Feature1/ui/view/HelloWorld.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
-struct HelloWorld: View {
-    var body: some View {
+public struct HelloWorld: View {
+    public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
